20bc7c0d429adbda99e7dd16db783475e21f807f
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-18 16:07:44 +0100

    inserito sia svolgimento con show/hide che addClass / removeClass

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
+// svolgimento con addClass / removeClass
+function showMenu(){
+  $(".hamburger-menu").addClass("active");
+}
+
+function hideMenu(){
+  $(".hamburger-menu").removeClass("active");
+}
+
+
+// svolgimento con SHOW/HIDE
 // function showMenu(){
-//   $(".hamburger-menu").addClass("active");
+//   $(".hamburger-menu").show();
 // }
 //
 // function hideMenu(){
 //   $(".hamburger-menu").hide();
 // }
-//
-// function init(){
-//   $(".header-right > a").click(showMenu);
-//   $(".close").click(hideMenu);
-// }
 
-function showMenu(){
-  $(".hamburger-menu").toggle();
-}
-
-function hideMenu(){
-  $(".hamburger-menu").hide();
-}
 
 function init(){
   $(".header-right > a").click(showMenu);
